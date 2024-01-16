bf41ce43e022da914cd53717e35bfeb3885cb23a
Divyansh Bhardwaj <divyanshb22@gmail.com>
2024-01-16 21:40:41 +0530

    added fields for SoftwareApplication

--- src/main/java/problem/model/SoftwareApplication.java
+++ src/main/java/problem/model/SoftwareApplication.java
@@ -1,4 +1,12 @@
+/*
+ * Created By: Divyansh Bhardwaj
+ * IDE: IntelliJ IDEA Ultimate
+ * Date: 16 Jan 2024
+ */
+
 package problem.model;
 
 public class SoftwareApplication {
+    private String applicationName;
+    private double cost;
 }
